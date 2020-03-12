505744e10811b8c2283c557033bdc3677e493212
zhaojintian <729234283@qq.com>
2020-03-12 22:21:16 +0800

    优化

--- src/crud.controller.ts
+++ src/crud.controller.ts
@@ -43,7 +43,7 @@ export class CrudController {
                   model: this.include[item.model]
                 }))
               : [],
-          order: order[0] ? order : [["id", "DESC"]]
+          order: order && order[0] ? order : [["id", "DESC"]]
         })
       }
     });
