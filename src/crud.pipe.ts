bff4307f46405c0654e1c6041431e4fe9bc0bb66
729234283 <729234283@qq.com>
2020-12-05 21:07:53 +0800

    upadte

---
+++ src/crud.pipe.ts
@@ -0,0 +1,60 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException
+} from "@nestjs/common";
+
+@Injectable()
+export class CrudPipe implements PipeTransform {
+  transform(value, { type, data }: ArgumentMetadata) {
+    switch (type) {
+      case "query": {
+        ["where", "include", "order", "attributes"].forEach(key => {
+          CrudPipe.toObj(key, value);
+        });
+
+        ["offset", "limit"].forEach(key => {
+          CrudPipe.toInt(key, value);
+        });
+
+        return value;
+      }
+
+      case "param": {
+        if (data === "id") {
+          if (isNaN(value)) {
+            throw new BadRequestException("id 参数错误");
+          }
+
+          return parseInt(value, 10);
+        }
+
+        return value;
+      }
+
+      default:
+        return value;
+    }
+  }
+
+  static toObj(key, value) {
+    if (value[key]) {
+      try {
+        value[key] = JSON.parse(value[key]);
+      } catch (e) {
+        throw new BadRequestException(`${key} 参数错误`);
+      }
+    }
+  }
+
+  static toInt(key, value) {
+    if (value[key]) {
+      if (isNaN(value[key])) {
+        throw new BadRequestException(`${key} 参数错误`);
+      } else {
+        value[key] = parseInt(value[key]);
+      }
+    }
+  }
+}
